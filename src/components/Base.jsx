b0dfe59f7c339d23163100989543feae073d803a
minazaev <minazaev3256@gmail.com>
2023-10-04 13:49:19 +0300

    first commit

---
+++ src/components/Base.jsx
@@ -0,0 +1,10 @@
+function Base(){
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <img src="https://images.squarespace-cdn.com/content/v1/597cc6e5f14aa1fbe78b9c25/075e456a-24aa-4c29-ab92-86c6f746b653/04_Burj+Khalifa.jpeg"/>
+        </div>
+    )
+}
+
+export default Base;
